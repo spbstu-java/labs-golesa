8e469e880c14eedec1f703eb558fabb3ade64c3c
unknown <yuragolesa@yandex.ru>
2025-09-23 18:18:44 +0300

    lab_1

--- lab_1/Main.java
+++ lab_1/Main.java
@@ -1,58 +1,92 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Hero hero1 = new Hero();
-        hero1.move(1,2);
+        Scanner scan = new Scanner(System.in);;
+        int from = 0;
+        int to = from;
 
-        hero1.setMoveStrat(new HorseRideStrategy());
-        hero1.move(2,3);
-
-        (new Hero(new FlyStrategy())).move(4,5);
+        System.out.println("Move strategy: " + hero1.moveStratName);
+        while (true) {
+            System.out.print("Change move strategy - c | Move - m | Quit - q --> ");
+            switch (scan.nextLine().charAt(0)) {
+                case 'c':
+                    System.out.print("Walk - 1 | Horse ride - 2 | Fly - 3 --> ");
+                    hero1.setMoveStrat(
+                        switch (scan.hasNextInt() ? scan.nextInt() : 1) {
+                            case 2 -> new HorseRideStrategy();
+                            case 3 -> new FlyStrategy();
+                            default -> new WalkStrategy();
+                        }
+                    );
+                    System.out.println("Move strategy changed to " + hero1.moveStratName + "\n");
+                    scan.nextLine();
+                break;
+                case 'm':
+                    System.out.print("Move to (int) --> ");
+                    to = scan.hasNextInt() ? scan.nextInt() : from;
+                    hero1.move(from, to);
+                    from = to;
+                    System.out.println();
+                    scan.nextLine();
+                break;
+                case 'q':
+                    return;
+            }
+        }
     }
-    
+
     interface MoveStrategy {
+        String getName();
         void move(int from, int to);
     }
 
     static class WalkStrategy implements MoveStrategy {
+        public String getName() {
+            return "Walk";
+        }
+
         public void move(int from, int to) {
-            System.out.println("Walk from " + from + " to " + to);
+            System.out.println("Hero walk from " + from + " to " + to);
         }
     }
 
     static class HorseRideStrategy implements MoveStrategy {
+        public String getName() {
+            return "Horse ride";
+        }
+
         public void move(int from, int to) {
-            System.out.println("Ride horse from " + from + " to " + to);
+            System.out.println("Hero ride horse from " + from + " to " + to);
         }
     }
 
     static class FlyStrategy implements MoveStrategy {
+        public String getName() {
+            return "Fly";
+        }
+
         public void move(int from, int to) {
-            System.out.println("Fly from " + from + " to " + to);
+            System.out.println("Hero fly from " + from + " to " + to);
         }
     }
 
     static class Hero {
-        private static byte n = 0;
-        private final String name_ = "Hero " + ++n;
-
         private MoveStrategy moveStrat_;
+        public String moveStratName;
 
         public Hero() {
-            moveStrat_ = new WalkStrategy();
-        }
-
-        public Hero(MoveStrategy moveStrat) {
-            moveStrat_ = moveStrat;
+            setMoveStrat(new WalkStrategy());
         }
 
         public void setMoveStrat(MoveStrategy moveStrat) {
             moveStrat_ = moveStrat;
+            moveStratName = moveStrat_.getName();
         }
 
         public void move(int from, int to) {
-            System.out.print(name_ + " -> ");
             moveStrat_.move(from, to);
         }
     }
-
 }
