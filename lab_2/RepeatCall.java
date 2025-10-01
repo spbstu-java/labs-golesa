fde5b1e7f481be1b093bbfe62bb2270266e72a9d
unknown <yuragolesa@yandex.ru>
2025-10-01 18:26:02 +0300

    lab_2

---
+++ lab_2/RepeatCall.java
@@ -0,0 +1,12 @@
+package lab2;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface RepeatCall {
+    int value() default 1;
+}
