c60d66d315dc1733ad0d69103af1cc223ce7e681
unknown <yuragolesa@yandex.ru>
2025-10-20 19:19:29 +0300

    lab_3

--- lab_3/Main.java
+++ lab_3/Main.java
@@ -1,13 +1,24 @@
 package lab3;
-
-import java.io.IOException;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Translator.InvalidFileFormatException, IOException {
-        System.out.print("Text(eng): ");
+    public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        Translator tr = new Translator("dict.txt");
+        Translator tr;
+        while(true) {
+            try{
+                System.out.print("Dictionary file: ");
+                tr = new Translator(scan.nextLine());
+                break;
+            }catch(Translator.FileReadException err) {
+                System.out.println("Error reading file: " + err.getMessage() + "\n");
+            }
+            catch(Translator.InvalidFileFormatException err) {
+                System.out.println(err.getMessage() + "\n");
+            }
+        }
+
+        System.out.print("Text(eng): ");
         String text = scan.nextLine();
         System.out.print("Text(ru) : " + tr.translate(text));
     }
